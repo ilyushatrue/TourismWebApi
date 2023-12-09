3d11984da0dc81d431b99a6d0f5fc8f63ed31013
itruhin <i.truhin@cmachine.ru>
2023-12-09 10:07:19 +1000

    jwt настройка

--- TourismWebApi/Program.cs
+++ TourismWebApi/Program.cs
@@ -1,21 +1,77 @@
 global using DataAccess.Data;
 using DataAccess.DbAccess;
+using DataAccess.Models.User.DTOs;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Reflection;
 using System.Text;
+using System.Security.Claims;
 using TourismWebApi.Configurations;
 using TourismWebApi.Endpoints;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+var securityScheme = new OpenApiSecurityScheme()
+{
+    Name = "Authentication",
+    Type = SecuritySchemeType.ApiKey,
+    Scheme = "Bearer",
+    BearerFormat = "JWT",
+    In = ParameterLocation.Header,
+    Description = "JWT authentication for API"
+};
+
+var securityRequirements = new OpenApiSecurityRequirement()
+{
+    {
+        new OpenApiSecurityScheme()
+        {
+            Reference = new OpenApiReference()
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        },
+        Array.Empty<string>()
+    }
+};
+
+var contactInfo = new OpenApiContact()
+{
+    Name = "Ilya",
+    Email = "@email"
+};
+
+var license = new OpenApiLicense()
+{
+    Name = "Free License",
+};
+
+var info = new OpenApiInfo()
+{
+    Version = "1.0.0",
+    Title = "Tourism API",
+    Description = "Tourism API",
+    Contact = contactInfo,
+    License = license
+};
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", info);
+    options.AddSecurityDefinition("Bearer", securityScheme);
+    options.AddSecurityRequirement(securityRequirements);
+});
+
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-builder.Services.AddScoped<IDbAccess,DbAccess>();
+builder.Services.AddScoped<IDbAccess, DbAccess>();
 builder.Services.AddScoped<UserData>();
 
 builder.Services.Configure<Jwt>(builder.Configuration.GetSection("Jwt"));
@@ -32,16 +88,17 @@ builder.Services.AddAuthentication(options =>
     jwt.SaveToken = true;
     jwt.TokenValidationParameters = new TokenValidationParameters()
     {
-        ValidateIssuerSigningKey = true,
+        ValidateIssuerSigningKey = false,
         IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = true, 
+        ValidateIssuer = false,
         ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidateAudience = true, 
+        ValidateAudience = false,
         ValidAudience = builder.Configuration["Jwt:Audience"],
         RequireExpirationTime = false,
         ValidateLifetime = false,
     };
 });
+builder.Services.AddAuthentication();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -52,14 +109,43 @@ if (app.Environment.IsDevelopment())
 }
 
 
-//app.MapPost("/accounts/login", [AllowAnonymous] () =>
-//{
+app.MapPost("/account/login", [AllowAnonymous] (UserInsertDto user) =>
+{
+    if (user.FirstName == "Ilya" && user.Password == "qwer1234")
+    {
+        var secret = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]!);
+        var issuer = builder.Configuration["Jwt:Issuer"];
+        var audience = builder.Configuration["Jwt:Audience"];
+        var securityKey = new SymmetricSecurityKey(secret);
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
-//});
+        var jwtTokenHandler = new JwtSecurityTokenHandler();
+
+        var tokenDescriptor = new SecurityTokenDescriptor()
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim("Id", "1"),
+                new Claim(JwtRegisteredClaimNames.Sub, user.FirstName),
+                new Claim(JwtRegisteredClaimNames.Email, user.FirstName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            }),
+            Expires = DateTime.Now.AddMinutes(6),
+            Audience = audience,
+            Issuer = issuer,
+            SigningCredentials = credentials
+        };
+
+        var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+        var jwtToken = jwtTokenHandler.WriteToken(token);
+        return Results.Ok(jwtToken);
+    }
+    return Results.Unauthorized();
+});
+app.UseAuthentication();
+app.UseAuthorization();
 app.AddUserEndpoints();
 
 app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.Run();
